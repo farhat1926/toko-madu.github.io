54bc3e5c8fe4e28ca2acfd5570bca4641e8c1860
farhat1926 <farhatmoh2000@gmail.com>
2023-08-03 15:01:31 +0700

    pertama

---
+++ script.js
@@ -0,0 +1,16 @@
+//toggle class active
+const navbarNav = document.querySelector('.navbar-nav');
+//ketika hamburger menu di click
+document.querySelector('#hamburger-menu').onclick = () => {
+	navbarNav.classList.toggle('active');
+};
+
+//klik diluar sidebar untuk menghilangkan nav
+const hamburger =document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function(e){
+	if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)){
+		navbarNav.classList.remove('active');
+	}
+
+});
